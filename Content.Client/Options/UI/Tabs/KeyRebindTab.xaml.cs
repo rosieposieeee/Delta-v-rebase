1a539bf4500ce024eb153ba668909295686588f9
poemota <142114334+poeMota@users.noreply.github.com>
2024-12-29 16:35:44 +1000

    mapping thing
    squashma

--- Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
@@ -281,6 +281,7 @@ namespace Content.Client.Options.UI.Tabs
             AddButton(EngineKeyFunctions.EditorRotateObject);
             AddButton(ContentKeyFunctions.EditorFlipObject);
             AddButton(ContentKeyFunctions.EditorCopyObject);
+            AddButton(ContentKeyFunctions.MappingEnablePick);
 
             AddHeader("ui-options-header-dev");
             AddButton(EngineKeyFunctions.ShowDebugConsole);
